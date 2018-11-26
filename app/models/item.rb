11f4e3bdfd7d4af34eb40f7afceb6cb68a22f66b
Olivier <orohellec44@gmail.com>
2018-11-27 00:57:22 +0100

    table_order ok

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   has_many :carts_items
   has_many :carts, through: :carts_items
+  has_many :orders, through: :order_items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_150259) do
+ActiveRecord::Schema.define(version: 2018_11_26_234032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,22 @@ ActiveRecord::Schema.define(version: 2018_11_26_150259) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "orders_items", force: :cascade do |t|
+    t.bigint "orders_id"
+    t.bigint "items_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["items_id"], name: "index_orders_items_on_items_id"
+    t.index ["orders_id"], name: "index_orders_items_on_orders_id"
   end
 
   create_table "users", force: :cascade do |t|
