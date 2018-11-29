47d2cc1ff09b0dd5101f32da6857512294d76ecb
Olivier <orohellec44@gmail.com>
2018-11-29 20:10:14 +0100

    fuck heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "ecommerce_#{Rails.env}"
 
-  
+  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -93,5 +93,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
 end
