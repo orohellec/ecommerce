22a755f3b1f60aa6744dde84a821e870a2d78867
Olivier <orohellec44@gmail.com>
2018-11-29 20:17:02 +0100

    fuck heroku2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV[‘RAILS_SERVE_STATIC_FILES’].present?
+  config.public_file_server.enabled = ENV[‘RAILS_SERVE_STATIC_FILES’]
 
   # Compress JavaScripts and CSS.
 
@@ -93,5 +93,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.initialize_on_precompile = true
+  config.assets.initialize_on_precompile = false
 end
